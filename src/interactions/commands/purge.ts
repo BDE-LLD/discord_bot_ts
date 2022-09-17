dfbeab3b13af1930bb27893cb3e8503ab91f2827
dhubleur <dhubleur@student.42.fr>
2022-09-17 19:00:08 +0200

    feat(CMD): Add purge command
    The purge command remove not valid authentifed users

---
+++ src/interactions/commands/purge.ts
@@ -0,0 +1,134 @@
+import { CommandInteraction, EmbedBuilder } from "discord.js";
+import { Client, Discord, Guard, Slash } from "discordx";
+import { guild_id, auth, roles } from "../../config.json";
+import { Client as Client42} from "42.js";
+
+@Discord()
+class Purge {
+    @Slash({
+        name: "purge",
+        description: "Clean all the users",
+    })
+    async purge(interaction: CommandInteraction, client: Client) {
+        await interaction.reply("Start cleaning...");
+        const guild = await client.guilds.fetch(guild_id);
+		await guild.members.fetch();
+		const students = await guild.roles.fetch(auth.roles.student);
+        const pisciners = await guild.roles.fetch(auth.roles.pisciner);
+        interaction.editReply({content: `Cleaning ${(students?.members?.size || 0) + (pisciners?.members?.size || 0)} users...`});
+        const client42 = new Client42(
+            <string>process.env.CLIENT_ID,
+            <string>process.env.CLIENT_SECRET
+        );
+        let remove_pisciners = 0;
+        let remove_students = 0;
+        if(pisciners?.members)
+        {
+            for(const user of pisciners?.members) {
+                if(user[1].roles.cache.find((role) => role.id === roles.bde))
+                {
+                    continue;
+                }
+                const name = user[1].nickname || user[1].user.username;
+                const login = name.match(/ \((.*)\)/);
+                if(login && login.length > 0)
+                {           
+                    const user42 = await client42.users.get(login[1]);
+                    if(!user42)
+                    {
+                        await user[1].roles.remove(auth.roles.pisciner);
+                        await user[1].roles.add(auth.roles.extern);
+                        remove_pisciners++;
+                        continue;
+                    }
+                    const cursus = user42.cursus_users.find((cursus) => cursus.cursus_id == 9);
+                    if(!cursus)
+                    {
+                        await user[1].roles.remove(auth.roles.pisciner);
+                        await user[1].roles.add(auth.roles.extern);
+                        remove_pisciners++;
+                        continue;
+                    }
+                    const today = new Date();
+                    const end = new Date(cursus.end_at);
+                    if(today > end)
+                    {
+                        await user[1].roles.remove(auth.roles.pisciner);
+                        await user[1].roles.add(auth.roles.extern);
+                        remove_pisciners++;
+                        continue;
+                    }
+                    continue;
+                }
+                else
+                {
+                    await user[1].roles.remove(auth.roles.pisciner);
+                    await user[1].roles.add(auth.roles.extern);
+                    remove_pisciners++;
+                    continue;
+                }
+            }
+        }
+        if(students?.members)
+        {
+            for(const user of students?.members) {
+                const name = user[1].nickname || user[1].user.username;
+                const login = name.match(/ \((.*)\)/);
+                const bde = user[1].roles.cache.find((role) => role.id === roles.bde);
+                if(login && login.length > 0)
+                {           
+                    const user42 = await client42.users.get(login[1]);
+                    if(!user42 && bde)
+                    {
+                        continue;
+                    }
+                    if(!user42)
+                    {
+                        await user[1].roles.remove(auth.roles.student);
+                        await user[1].roles.add(auth.roles.extern);
+                        remove_students++;
+                        continue;
+                    }
+                    const cursus = user42.cursus_users.find((cursus) => cursus.cursus_id == 21);
+                    if(!cursus && bde)
+                    {
+                        continue;
+                    }
+                    if(!cursus)
+                    {
+                        await user[1].roles.remove(auth.roles.student);
+                        await user[1].roles.add(auth.roles.extern);
+                        remove_students++;
+                        continue;
+                    }
+                    const today = new Date();
+                    const end = new Date(cursus.blackholed_at);
+                    if(today > end && !bde)
+                    {
+                        await user[1].roles.remove(auth.roles.student);
+                        await user[1].roles.add(auth.roles.extern);
+                        remove_students++;
+                        continue;
+                    }
+                    for(const coa of auth.coalitions) {
+                        if(user[1].roles.cache.find((role) => role.id == coa.role))
+                        {
+                            await user[1].roles.add(coa.role);
+                            let theoric_name = `${user42.usual_first_name || user42.first_name} (${user42.login}) ${coa.emoji}`;
+                            if(name != theoric_name)
+                            {
+                                await user[1].setNickname(theoric_name);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        interaction.editReply({content: `Cleaning done, ${remove_pisciners} pisciners and ${remove_students} students removed.`});
+    }
+}
+
+export async function init(client: Client) {
+    await client.initApplicationCommands();
+    console.log("Purge command inited");
+}
--- src/interactions/interactions_manager.ts
+++ src/interactions/interactions_manager.ts
@@ -3,10 +3,12 @@ import { init as auth_button_init } from "./buttons/auth_button";
 import { init as user_info_init } from "./commands/user_info";
 import { init as help_init } from "./commands/help";
 import { init as info_init } from "./commands/info";
+import { init as purge_init } from "./commands/purge";
 
 export async function init(client: Client) {
     await auth_button_init();
     await user_info_init(client);
     await help_init(client);
     await info_init(client);
+    await purge_init(client);
 }
