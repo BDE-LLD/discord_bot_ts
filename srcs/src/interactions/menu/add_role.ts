48dc6c98835ff9b0788b8a32738bc7dd2d6e9d16
SI BDE 42 <si@bde.student42.fr>
2023-04-24 17:31:52 +0000

    fix(ROLES): Repair users fetching

--- srcs/src/interactions/menu/add_role.ts
+++ srcs/src/interactions/menu/add_role.ts
@@ -26,10 +26,13 @@ class AddRole {
 			return;
 		}
 		interaction.deferReply();
-		let found = [];
-		let notFound = [];
+		let found: string[] = [];
+		let notFound: string[] = [];
+		const guildMembers = await interaction.guild?.members.fetch();
 		for (const user of users) {
-			const member = interaction.guild?.members.cache.find((member) => {
+			if (!guildMembers)
+				return false;
+			const member = guildMembers.find((member) => {
 				const login = member.displayName.match(/ \((.*)\)/);
 				if (login) {
 					return login[1] === user;
@@ -44,26 +47,28 @@ class AddRole {
 				continue;
 			}
 			found.push(member.displayName);
-			member.roles.add(role);
+			await member.roles.add(role);
 		}
-		const embed = new EmbedBuilder()
-			.setColor("#00C851")
-			.setTitle("Rôle ajouté")
-			.setDescription(
-				"Le rôle a été ajouté à " +
-					found.length +
-					" utilisateurs.\n\n" +
-					notFound.length +
-					" utilisateurs n'ont pas été trouvés."
-			)
-			.setFields([
-				{
-					name: "Utilisateurs non trouvés",
-					value: notFound.join("\n"),
-					inline: true,
-				},
-			]);
-		interaction.reply({ embeds: [embed] });
+		setTimeout(() => {
+			const embed = new EmbedBuilder()
+				.setColor("#00C851")
+				.setTitle("Rôle ajouté")
+				.setDescription(
+					"Le rôle a été ajouté à " +
+						found.length +
+						" utilisateurs.\n\n" +
+						notFound.length +
+						" utilisateurs n'ont pas été trouvés."
+				)
+				.setFields([
+					{
+						name: "Utilisateurs non trouvés",
+						value: notFound.join("\n"),
+						inline: true,
+					},
+				]);
+			interaction.editReply({ embeds: [embed] });
+		}, 1000);
 	}
 }
 
